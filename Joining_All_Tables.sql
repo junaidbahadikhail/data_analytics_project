71ac8b7aa955e0bc374da914b40bc6a299939a37
Junaid Khan <45282292+junaidbahadikhail@users.noreply.github.com>
2023-12-20 21:00:43 +0500

    Add files via upload

---
+++ Joining_All_Tables.sql
@@ -0,0 +1,122 @@
+-- Prepare phase --
+-- Top 1000 Records ---
+Select 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202201]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+
+FROM dbo.[202202]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202203]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202204]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202205]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202206]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202207]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202208]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202209]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202210]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202211]
+UNION ALL
+SELECT
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name, 
+member_casual
+FROM dbo.[202212]
